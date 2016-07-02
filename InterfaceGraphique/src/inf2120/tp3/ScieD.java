0503beea7d9aa1caac69898977e56d76c0cacd23
Gabriel Gamy <gabrieyakpaoro@gmail.com>
2016-07-01 23:39:08 -0400

    solution premiere question

---
+++ InterfaceGraphique/src/inf2120/tp3/ScieD.java
@@ -0,0 +1,8 @@
+package inf2120.tp3;
+
+public class ScieD extends TriangleGenerique {
+	ScieD( double tauxEchantillonage, double frequence ) 
+	{
+		super( tauxEchantillonage, frequence, 0.0 );
+	}
+}
