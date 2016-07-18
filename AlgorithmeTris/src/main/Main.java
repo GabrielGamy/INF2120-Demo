e24865e70b3913307f90c4e0121d0baa218b0807
Gabriel Gamy <gabrieyakpaoro@gmail.com>
2016-07-18 19:15:04 -0400

    Tri par insertion

---
+++ AlgorithmeTris/src/main/Main.java
@@ -0,0 +1,11 @@
+
+package main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int [] liste1 = {12, 4, 7, 3, 6, 8, 2, 10, 9}; 
+        
+        Tris.trierInsertion(liste1);
+    }    
+}
